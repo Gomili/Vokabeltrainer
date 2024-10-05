47a5e1f70cf8c7d8c90cc5e24ac456c44d44ae88
Michael Goerigk <michael.goerigk@outlook.de>
2024-10-05 11:11:40 +0200

    Add Sqlite

--- Vokabeltrainer.Core/Models/Vokabel.cs
+++ Vokabeltrainer.Core/Models/Vokabel.cs
@@ -2,6 +2,7 @@
 
 public class Vokabel
 {
+    public Guid Id { get; set; }
     public string Deutsch { get; set; }
     public string Englisch { get; set; }
     public int Zaehler { get; set; }
---
+++ Vokabeltrainer.Core/VokabelContext/VokabelDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vokabeltrainer.Core.Models;
+
+namespace Vokabeltrainer.Core.VokabelContext;
+
+public class VokabelDataContext : DbContext
+{
+    public DbSet<Vokabel> Vokabeln { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Vokabel.db");
+    }
+}
