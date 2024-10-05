515588986edb76b5668595c1d28f8310543bf171
Michael Goerigk <michael.goerigk@outlook.de>
2024-10-05 12:14:59 +0200

    Add Dataservice

---
+++ Vokabeltrainer.Core/Contracts/Services/IDataService.cs
@@ -0,0 +1,14 @@
+﻿using Vokabeltrainer.Core.Models;
+
+namespace Vokabeltrainer.Core.Contracts.Services;
+
+public interface IDataService
+{
+    Task<List<Vokabel>> ReadAllAsync();
+
+    Task<Vokabel?> ReadAsync(Guid id);
+    
+    Task SaveAsync(Vokabel content);
+
+    Task DeleteAsync(Vokabel content);
+}
---
+++ Vokabeltrainer.Core/Services/DataService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vokabeltrainer.Core.Contracts.Services;
+using Vokabeltrainer.Core.Models;
+using Vokabeltrainer.Core.VokabelContext;
+
+namespace Vokabeltrainer.Core.Services;
+
+public class DataService : IDataService
+{
+
+    public DataService()
+    {
+        using var context = new VokabelDataContext();
+        context.Database.Migrate();
+    }
+    
+    public async Task<List<Vokabel>> ReadAllAsync()
+    {
+        await using var context = new VokabelDataContext();
+        return await context.Vokabeln.ToListAsync();
+    }
+
+    public async Task<Vokabel?> ReadAsync(Guid id)
+    {
+        await using var context = new VokabelDataContext();
+        return await context.Vokabeln.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task SaveAsync(Vokabel content)
+    {
+        await using var context = new VokabelDataContext();
+        if (content.Id == Guid.Empty)
+        {
+            content.Id = Guid.NewGuid();
+            context.Vokabeln.Add(content);
+        }
+        else
+        {
+            context.Vokabeln.Update(content);
+        }
+
+        await context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Vokabel content)
+    {
+        await using var context = new VokabelDataContext();
+        context.Vokabeln.Remove(content);
+        await context.SaveChangesAsync();
+    }
+}
