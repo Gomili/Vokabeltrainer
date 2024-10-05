c1e00b190a287bffe0c80a44623d73e96bce64c7
Michael Goerigk <michael.goerigk@outlook.de>
2024-10-05 11:26:27 +0200

    Add Sqlite Database

---
+++ Vokabeltrainer.Core/Migrations/20241005092440_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vokabeltrainer.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vokabeln",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Deutsch = table.Column<string>(type: "TEXT", nullable: true),
+                    Englisch = table.Column<string>(type: "TEXT", nullable: true),
+                    Zaehler = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vokabeln", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vokabeln");
+        }
+    }
+}
