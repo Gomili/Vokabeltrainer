6e4bb2ea9166a611686b079e8c4d228ac8f83e1c
Michael Goerigk <michael.goerigk@outlook.de>
2024-10-05 12:45:42 +0200

    Laden der Daten aus der Datenbank

--- Vokabeltrainer/ViewModels/VokabeleingabeViewModel.cs
+++ Vokabeltrainer/ViewModels/VokabeleingabeViewModel.cs
@@ -1,25 +1,32 @@
 ﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Vokabeltrainer.Core.Contracts.Services;
 using Vokabeltrainer.Core.Models;
+using Vokabeltrainer.Core.Services;
+using Vokabeltrainer.Core.VokabelContext;
 
 namespace Vokabeltrainer.ViewModels;
 
 public partial class VokabeleingabeViewModel : ObservableRecipient
 {
+    private readonly IDataService _dataService;
     [ObservableProperty] private string _textDeutsch = string.Empty;
     [ObservableProperty] private string _textEnglisch = string.Empty;
     [ObservableProperty] private string _zaehler = string.Empty;
     [ObservableProperty] private ObservableCollection<Vokabel> _displayListe = new() ;
 
-    private List<Vokabel> _sourceListe = [];
+    private List<Vokabel> _sourceListe;
     
-    public VokabeleingabeViewModel()
+    public VokabeleingabeViewModel(IDataService dataService)
     {
-        _sourceListe.Add(new Vokabel(){Deutsch = "Baum",Englisch = "tree",Zaehler = 100});
-        _sourceListe.Add(new Vokabel(){Deutsch = "Tisch",Englisch = "table",Zaehler = 100});
-        _sourceListe.Add(new Vokabel(){Deutsch = "reisen",Englisch = "to travel",Zaehler = 100});
+        _dataService = dataService;
+        
+        _sourceListe = _dataService.ReadAllAsync().GetAwaiter().GetResult();
 
         foreach (Vokabel vokabel in _sourceListe)
             DisplayListe.Add(vokabel);
     }
+
+    
 }
