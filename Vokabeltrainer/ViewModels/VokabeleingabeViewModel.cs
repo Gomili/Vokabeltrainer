1e67e315fb663e78b21ad92fe8fa642517b3ff9d
Michael Goerigk <michael.goerigk@outlook.de>
2024-10-05 10:28:23 +0200

    Ausgabenliste zugefügt

--- Vokabeltrainer/ViewModels/VokabeleingabeViewModel.cs
+++ Vokabeltrainer/ViewModels/VokabeleingabeViewModel.cs
@@ -10,8 +10,16 @@ public partial class VokabeleingabeViewModel : ObservableRecipient
     [ObservableProperty] private string _textEnglisch = string.Empty;
     [ObservableProperty] private string _zaehler = string.Empty;
     [ObservableProperty] private ObservableCollection<Vokabel> _displayListe = new() ;
+
+    private List<Vokabel> _sourceListe = [];
     
     public VokabeleingabeViewModel()
     {
+        _sourceListe.Add(new Vokabel(){Deutsch = "Baum",Englisch = "tree",Zaehler = 100});
+        _sourceListe.Add(new Vokabel(){Deutsch = "Tisch",Englisch = "table",Zaehler = 100});
+        _sourceListe.Add(new Vokabel(){Deutsch = "reisen",Englisch = "to travel",Zaehler = 100});
+
+        foreach (Vokabel vokabel in _sourceListe)
+            DisplayListe.Add(vokabel);
     }
 }
