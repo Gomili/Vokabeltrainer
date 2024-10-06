668be476d64d078c68c9aee025ca2b5926e9289e
Michael Goerigk <michael.goerigk@outlook.de>
2024-10-06 18:26:35 +0200

    Session Ausgabe verbessert

---
+++ Vokabeltrainer/Converter/DateTimeToTimeConverter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace Vokabeltrainer.Converter;
+
+public class DateTimeToTimeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is TimeSpan timeSpan)
+        {
+            return timeSpan.ToString(@"hh\:mm\:ss");
+        }
+        if (value is DateTime dateTime)
+        {
+            return dateTime.ToString("HH:mm:ss");
+        }
+        return string.Empty;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
